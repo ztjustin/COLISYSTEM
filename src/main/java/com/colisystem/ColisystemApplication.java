a3a3578687fc9fb5032174b29f1398519d594dbb
Justin <justinzu93@gmail.com>
2018-03-25 18:57:24 -0600

    Iniciando la aplicacion

---
+++ src/main/java/com/colisystem/ColisystemApplication.java
@@ -0,0 +1,12 @@
+package com.colisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColisystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColisystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/colisystem/ColisystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.colisystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ColisystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
